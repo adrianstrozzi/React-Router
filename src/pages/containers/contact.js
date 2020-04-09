24b61724c3f8f46b3ac396ac097eb65c9258c32f
Adrian Strozzi <adrianstrozzi@gmail.com>
2020-04-08 19:27:21 -0500

    Created contact component and added it via Route in app.js

---
+++ src/pages/containers/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => (
+  <div className="Page">
+    <h1>Hola soy Adrian Strozzi </h1>
+    <p>Productor Musical y Programador de Desarollo Web en formación</p>
+    <p>Aquí puedes entrar a mi perfil de <a href="https://www.linkedin.com/in/adrianstrozzi/">Linkedin.</a></p>
+  </div>
+)
+
+export default Contact
